093964926d128a27ab453efd6f94ef2e324e4068
Toczak <patrpot@gmail.com>
2018-11-14 13:02:03 +0100

    First test commit

--- AlbumDB/Program.cs
+++ AlbumDB/Program.cs
@@ -17,6 +17,8 @@ namespace AlbumDB
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //test
         }
     }
 }

8a8da396ec138edfdf7b49c264e7e03023b2d85e
Patryk <Patryk@PATRYK-AIR>
2018-11-16 11:36:51 +0100

    Change design

--- AlbumDB/Form1.Designer.cs
+++ AlbumDB/Form1.Designer.cs
@@ -30,10 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.albumyDataSet = new AlbumDB.albumyDataSet();
-            this.albumyDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.albumBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.albumyDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.albumyDataSet = new AlbumDB.albumyDataSet();
             this.albumTableAdapter = new AlbumDB.albumyDataSetTableAdapters.albumTableAdapter();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.albumBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.albumyDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.albumBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.albumBindingSource3 = new System.Windows.Forms.BindingSource(this.components);
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.idzespoluDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.idgatunekDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -44,9 +50,14 @@
             this.dlugoscDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.opisDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSet)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSetBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource3)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -64,29 +75,68 @@
             this.dlugoscDataGridViewTextBoxColumn,
             this.opisDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.albumBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(172, 139);
+            this.dataGridView1.Location = new System.Drawing.Point(1, 61);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(565, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(801, 390);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // albumyDataSet
-            // 
-            this.albumyDataSet.DataSetName = "albumyDataSet";
-            this.albumyDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // albumyDataSetBindingSource
-            // 
-            this.albumyDataSetBindingSource.DataSource = this.albumyDataSet;
-            this.albumyDataSetBindingSource.Position = 0;
             // 
             // albumBindingSource
             // 
             this.albumBindingSource.DataMember = "album";
             this.albumBindingSource.DataSource = this.albumyDataSetBindingSource;
             // 
+            // albumyDataSetBindingSource
+            // 
+            this.albumyDataSetBindingSource.DataSource = this.albumyDataSet;
+            this.albumyDataSetBindingSource.Position = 0;
+            // 
+            // albumyDataSet
+            // 
+            this.albumyDataSet.DataSetName = "albumyDataSet";
+            this.albumyDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
             // albumTableAdapter
             // 
             this.albumTableAdapter.ClearBeforeFill = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(802, 33);
+            this.panel1.TabIndex = 1;
+            // 
+            // albumBindingSource1
+            // 
+            this.albumBindingSource1.DataMember = "album";
+            this.albumBindingSource1.DataSource = this.albumyDataSetBindingSource;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.albumBindingSource1, "id", true));
+            this.comboBox1.DataSource = this.albumBindingSource1;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(627, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // albumyDataSetBindingSource1
+            // 
+            this.albumyDataSetBindingSource1.DataSource = this.albumyDataSet;
+            this.albumyDataSetBindingSource1.Position = 0;
+            // 
+            // albumBindingSource2
+            // 
+            this.albumBindingSource2.DataMember = "album";
+            this.albumBindingSource2.DataSource = this.albumyDataSetBindingSource1;
+            // 
+            // albumBindingSource3
+            // 
+            this.albumBindingSource3.DataMember = "album";
+            this.albumBindingSource3.DataSource = this.albumyDataSet;
             // 
             // idDataGridViewTextBoxColumn
             // 
@@ -147,14 +197,20 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSet)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumyDataSetBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumBindingSource3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -166,6 +222,8 @@
         private albumyDataSet albumyDataSet;
         private System.Windows.Forms.BindingSource albumBindingSource;
         private albumyDataSetTableAdapters.albumTableAdapter albumTableAdapter;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.BindingSource albumBindingSource1;
         private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn idzespoluDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn idgatunekDataGridViewTextBoxColumn;
@@ -175,6 +233,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn iloscpiosenekDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dlugoscDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn opisDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource albumyDataSetBindingSource1;
+        private System.Windows.Forms.BindingSource albumBindingSource2;
+        private System.Windows.Forms.BindingSource albumBindingSource3;
     }
 }
 

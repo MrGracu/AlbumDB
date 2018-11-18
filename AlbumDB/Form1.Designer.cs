21df94e1b1f80dba9d3e9d474d78529d821b1a2e
MrGracu <s23991@st.pwsz.nysa.pl>
2018-11-18 17:19:25 +0100

    Add new query to ComboBox and fixed some errors

--- AlbumDB/Form1.Designer.cs
+++ AlbumDB/Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -43,11 +43,13 @@
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 61);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(846, 390);
             this.dataGridView1.TabIndex = 0;
@@ -64,25 +66,26 @@
             this.panel1.Size = new System.Drawing.Size(846, 33);
             this.panel1.TabIndex = 1;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(685, 6);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(152, 21);
-            this.comboBox1.TabIndex = 0;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // comboBox2
             // 
             this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(523, 6);
+            this.comboBox2.Location = new System.Drawing.Point(424, 6);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(152, 21);
+            this.comboBox2.Size = new System.Drawing.Size(257, 21);
             this.comboBox2.TabIndex = 1;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(687, 6);
+            this.comboBox1.MaxDropDownItems = 10;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
@@ -91,7 +94,7 @@
             this.ClientSize = new System.Drawing.Size(844, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
-            this.MinimumSize = new System.Drawing.Size(344, 240);
+            this.MinimumSize = new System.Drawing.Size(441, 240);
             this.Name = "Form1";
             this.Text = "Albumy Muzyczne DB";
             this.Load += new System.EventHandler(this.Form1_Load);
--- AlbumDB/Form1.cs
+++ AlbumDB/Form1.cs
@@ -27,15 +27,19 @@ namespace AlbumDB
             {
                 conn.Open();
                 DataTable dt = conn.GetSchema("Tables", new string[] { null, null, null, "TABLE" });
-                foreach(DataRow row in dt.Rows)
+                foreach (DataRow row in dt.Rows)
                 {
                     comboBox1.Items.Add(row.Field<string>("TABLE_NAME"));
                 }
             }
+            comboBox2.Items.Add("album + zespol + wytwornia + gatunek + piosenka");
+            comboBox2.Items.Add("zespol + czlonek_zespolu + muzyk + stanowisko");
+            comboBox2.Items.Add("album + ocena + ocena_albumu");
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedIndex < 0 && comboBox2.SelectedIndex >= 0) return;
             string conString = @"Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=albumy_muz.mdb;" + "Persist Security Info=True;" + "Jet OLEDB:Database Password=myPassword;";
             string tableName = comboBox1.SelectedItem.ToString();
             using (OleDbConnection conn = new OleDbConnection(conString))
@@ -63,6 +67,28 @@ namespace AlbumDB
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (comboBox1.SelectedIndex >= 0 && comboBox2.SelectedIndex < 0) return;
+            string conString = @"Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=albumy_muz.mdb;" + "Persist Security Info=True;" + "Jet OLEDB:Database Password=myPassword;";
+            string[] commandTab = { "SELECT album.nazwa AS[Nazwa albumu], zespol.nazwa AS[Zespół], wytwornia.nazwa AS[Wytwórnia], gatunek.nazwa AS[Gatunek], piosenka.nr_piosenki AS[Nr piosenki], piosenka.tytul AS[Tytuł], piosenka.czas AS[Czas trwania] FROM((((album INNER JOIN piosenka ON album.id = piosenka.id_albumu) INNER JOIN wytwornia ON album.id_wytwornia = wytwornia.id) INNER JOIN gatunek ON album.id_gatunek = gatunek.id) INNER JOIN zespol ON album.id_zespolu = zespol.id) ORDER BY album.id",
+                                    "SELECT zespol.nazwa AS [Zespół], muzyk.imie AS [Imie], muzyk.nazwisko AS [Nazwisko], muzyk.data_urodzenia AS [Data urodzenia], stanowisko.nazwa AS [Stanowisko] FROM (((zespol INNER JOIN czlonek_zespolu ON zespol.id=czlonek_zespolu.id_zespolu) INNER JOIN muzyk ON czlonek_zespolu.id_muzyka=muzyk.id) INNER JOIN stanowisko ON czlonek_zespolu.id_stanowiska=stanowisko.id) ORDER BY zespol.id",
+                                    "SELECT album.nazwa AS [Nazwa albumu], ocena.wartosc AS [Ocena], ocena_albumu.recenzja AS [Recenzja] FROM ((album INNER JOIN ocena_albumu ON album.id = ocena_albumu.id_albumu) INNER JOIN ocena ON ocena_albumu.id_ocena = ocena.id) ORDER BY album.id" };
+            int selectedNumber = comboBox2.SelectedIndex;
+            using (OleDbConnection conn = new OleDbConnection(conString))
+            using (OleDbCommand cmd = new OleDbCommand(commandTab[selectedNumber], conn))
+            {
+                conn.Open();
+                using (OleDbDataAdapter adapter = new OleDbDataAdapter(cmd))
+                {
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "tab");
+                    dataGridView1.DataSource = ds.Tables[0];
+
+                    if (selectedNumber == 0)
+                    {
+                        dataGridView1.Columns[6].DefaultCellStyle.Format = "T";
+                    }
+                }
+            }
             comboBox1.SelectedIndex = -1;
         }
     }
